d6b63e2f6de4c9d878c22e2b8b7fe4d41e31768f
agenefriday <122647054+agenefriday@users.noreply.github.com>
2023-03-30 20:46:27 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/2b31e057-aebd-4b8f-b1c8-91ef6fefa8c8/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
